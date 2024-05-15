a0548e98896041f5e599486496ae04e2544378bf
Samuel Condack <samuelcondackxz@gmail.com>
2024-05-15 20:59:46 -0300

    feat: start secondSection - homeNoAuth

--- src/pages/homeNoAuth/index.jsx
+++ src/pages/homeNoAuth/index.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import styles from "./styles.module.scss";
+import upRight from "../../assets/upRight.png"
+import pink from "../../assets/pink.png"
 
 export default function HomeNoAuth() {
   return (
@@ -8,10 +10,10 @@ export default function HomeNoAuth() {
       <Header />
       <div className={styles.firstContent}>
         <div className={styles.title}>
-          <p>
+          <h1>
             Take charge of <br /> your money and <br />
             live your{" "}
-          </p>
+          </h1>
         </div>
         <div className={styles.subTitle}>
           <p>
@@ -22,6 +24,19 @@ export default function HomeNoAuth() {
           <Link className={styles.buttonLink} to="/">
             <button className={styles.registerBtn}>Try it now!</button>
           </Link>
+        </div>
+      </div>
+      <div className={styles.secondContent}>
+        <h1>Your everyday finance control app</h1>
+          <p>Take notes</p>
+        <div className={styles.notes}>
+          <div>
+            <div className={styles.arrowDiv}>
+              <img className={styles.upRightArrow} src={pink} alt="" />
+              <p>+3.1%</p>
+            </div>
+            <p>Outcome</p>
+          </div>
         </div>
       </div>
     </>
