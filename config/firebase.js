07e2599a01bb7548fae819993a2ccd40ac6afd8f
Samuel Condack <samuelcondackxz@gmail.com>
2024-05-21 09:28:49 -0300

    feat:add .env

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJuj4ZNwI2SYfN-UQTv7mtkZ2BqrbFT2w",
-  authDomain: "backendapp-38e8d.firebaseapp.com",
-  projectId: "backendapp-38e8d",
-  storageBucket: "backendapp-38e8d.appspot.com",
-  messagingSenderId: "378033328678",
-  appId: "1:378033328678:web:1b4abf8d7acb203b864ce7",
-  measurementId: "G-L8EGQ4NX9F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const googleProvider = new GoogleAuthProvider()
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
